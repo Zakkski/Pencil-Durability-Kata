cc4211b0272389c22d8a3338bce2dfd50d26332e
Zachary Kwiecinski <Zachary.Kwiecinski@gmail.com>
2019-07-03 20:36:09 +0200

    test for basic durability of the pencil

--- lib/pencil.rb
+++ lib/pencil.rb
@@ -1,5 +1,22 @@
 class Pencil
-  def write(text, input)
-    (text + " " + input).strip 
+  attr_reader :durability
+
+  def initialize(attributes = {})
+    @durability = attributes[:durability] || 10
+
   end
+
+  def write(text, input)
+    letters = input.split('').map { |letter| write_letter(letter) }.join('')
+    text + letters
+  end
+
+  def write_letter(letter)
+    return " " if @durability.zero?
+
+    @durability -= 1
+    return letter
+    # if letter =~ /\s/
+  end
+
 end
--- spec/pencil_spec.rb
+++ spec/pencil_spec.rb
@@ -1,20 +1,31 @@
 require_relative '../lib/pencil.rb'
 
 describe 'Pencil' do
-  pencil = Pencil.new
-  text = ""
-
   it 'should have a class of "Pencil"' do
     Object.const_defined?('Pencil')
   end
 
+  describe '#durability' do
+    it 'should return durability of the pencil' do
+      expect(Pencil.new).to respond_to :durability
+    end
+  end
+
   describe '#write' do
+    pencil = Pencil.new()
+
     it 'should be able to write text to a paper' do
       expect(pencil.write("", "World")).to eq("World")
     end
 
-    it 'should assume every input is that start of a new word (add a space before it begins)' do
-      expect(pencil.write("Hello", "World")).to eq("Hello World")
+    # it 'should assume every input is that start of a new word (add a space before it begins)' do
+    #   expect(pencil.write("Hello", "World")).to eq("Hello World")
+    # end
+
+    pencil5 = Pencil.new(durability: 5)
+
+    it 'should only be able to write characters if durability is greater than 0' do
+      expect(pencil5.write("", "worlds")).to eq("world ")
     end
   end
 end
